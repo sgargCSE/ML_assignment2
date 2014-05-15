9d99f1bb59560524742946bfd5afd4d4709d44aa
sgargCSE <sgarg@cse.unsw.edu.au>
2014-05-15 13:03:20 +1000

    Initial Commit
    Basic stuff

---
+++ src/LinearUnit.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class LinearUnit {
+	
+	
+	
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
+		String line = null;
+		ArrayList<double[]> data = new ArrayList<double[]>(100);
+		
+		while ((line = reader.readLine()) != null) {
+		    if (line.matches("^[0-9].*")){
+		    	//System.out.println(line);
+		    	String s = line;
+		    	double[] a = new double[8];
+		    	String[] split = s.split(",");
+		    	//System.out.println(split.length);
+		    	System.out.println(split[3]);
+		    	for (int i = 0;i<split.length;i++){
+		    		a[i] = Double.parseDouble(split[i]);
+		    		System.out.print(split[i] + " ");
+		    	}
+		    	data.add(a);
+		    }
+		}
+		double[] weights = new double[8];
+		Random r = new Random();
+		for (int i = 0;i<8;i++){
+			weights[i] = r.nextDouble();
+		}
+		
+		
+		
+		for (double[] a : data) {
+			double pred = sumProduct(weights, a);
+			if (pred != a[0]){
+				//update the weights
+			}
+		}
+	}
+	
+	public static double sumProduct(double[] w, double [] data){
+		double sum = w[0];
+		for (int i = 1;i<data.length;i++){
+			sum += w[i] * data[i];
+		}
+		return sum;
+	}
+}
+
+
