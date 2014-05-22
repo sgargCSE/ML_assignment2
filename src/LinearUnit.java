52f26cc1ef2bc7e48e1776a4f178ebe7722600ed
sgargCSE <sgarg@cse.unsw.edu.au>
2014-05-22 14:32:14 +1000

    Isn't so flip floppy, closer but no cake.

--- src/LinearUnit.java
+++ src/LinearUnit.java
@@ -19,7 +19,7 @@ public class LinearUnit {
 		BufferedReader reader = new BufferedReader(new FileReader("data.txt"));
 		String line = null;
 		ArrayList<double[]> data = new ArrayList<double[]>(100);
-
+		//double[] average = new double[8]; 
 		while ((line = reader.readLine()) != null) {
 			if (line.matches("^[0-9].*")){
 				//System.out.println(line);
@@ -31,6 +31,7 @@ public class LinearUnit {
 				for (int i = 0;i<split.length;i++){
 					try {
 						a[i] = Double.parseDouble(split[i]);
+						//average[i] += a[i];
 					} catch (NumberFormatException e){ 
 						a[i] = -1;
 					}
@@ -40,55 +41,56 @@ public class LinearUnit {
 				data.add(a);
 			}
 		}
+		
+/*		for (int i=0;i<average.length;i++){
+			average[i] = average[i]/data.size();
+		}*/
 
 		double[] weights = new double[8];
-		Random r = new Random();
+		Random r = new Random(100);
 		for (int i = 0;i<8;i++){
 			weights[i] = r.nextDouble();
-			System.out.printf("%.2f ", weights[i]);
+			//System.out.printf("%.2f ", weights[i]);
 		}
-		System.out.println();
+		
+		//System.out.println();
 
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < data.size(); i++) {
 			double[] newWeights = new double[8];
 			double[] predictions = new double[i+1];
 			for (int w = 0; w<weights.length; w++){
 				double wupdate = 0;
 				for (int j = 0; j<= i; j++){
 					double[] sample = data.get(j);
-					for (int iii=0;iii<sample.length;iii++) System.out.print(sample[iii]+" ");
+					//for (int iii=0;iii<sample.length;iii++) System.out.print(sample[iii]+" ");
 					//System.out.println(sample);
-					if (j == 0){
+					if (w == 0){
 						predictions[j] = sumProduct(weights, sample);
+						System.out.println((int)predictions[j] + " -> " + (int)sample[7]);
 					}
 					double pred = predictions[j];
-					if (((int)pred) != sample[7]){
-						//update the weights
-						System.out.println((int)pred + " -> " + (int)sample[7]);
-						if (w == 0){
-							wupdate += (sample[7] - pred) * -1;
-						}else{
-							wupdate += (sample[7] - pred) * -sample[w-1];							
-						}
+					if (w == 0){
+						wupdate += (- sample[7] + pred) * 1;
 					}else{
-						//we predicted correctly
+						wupdate += (- sample[7] + pred) * (sample[w-1]);							
 					}
-
 				}
-				newWeights[w] = weights[w] - 0.1 * wupdate;
+				newWeights[w] = weights[w] - 0.0000001 * 1/(i+1) * wupdate;
 				//System.out.printf("%.2f ", weights[w]);
 			}
 			weights = newWeights.clone();
 			for (int iii=0;iii<weights.length;iii++) System.out.print(weights[iii]+" ");
-			System.out.println("------------------------");
+			System.out.println("\n------------------------");
 		}
 	}
 
 	public static double sumProduct(double[] w, double [] data){
 		double sum = w[0];
-		for (int i = 1;i<(data.length-1);i++){
+		for (int i = 1;i<8;i++){
+			//System.out.println(sum+"+=" + w[i] + " * " + data[i-1]);
 			sum += w[i] * data[i-1];
 		}
+		//System.out.println("F:"+sum);
 		return sum;
 	}
 }
