966aef3a1b61015eae148e7318bedbe0870accbb
sgargCSE <sgarg@cse.unsw.edu.au>
2014-05-22 16:20:08 +1000

    Takes max, and adjusts the samples to be roughly in the range -1 to 1.

--- src/LinearUnit.java
+++ src/LinearUnit.java
@@ -7,9 +7,6 @@ import java.util.Random;
 
 
 public class LinearUnit {
-
-
-
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -20,6 +17,7 @@ public class LinearUnit {
 		String line = null;
 		ArrayList<double[]> data = new ArrayList<double[]>(100);
 		double[] average = new double[8]; 
+		double[] max = new double[8];
 		while ((line = reader.readLine()) != null) {
 			if (line.matches("^[0-9].*")){
 				//System.out.println(line);
@@ -32,6 +30,9 @@ public class LinearUnit {
 					try {
 						a[i] = Double.parseDouble(split[i]);
 						average[i] += a[i];
+						if (a[i] > max[i]){
+							max[i] = a[i];
+						}
 					} catch (NumberFormatException e){ 
 						a[i] = -1;
 					}
@@ -44,6 +45,7 @@ public class LinearUnit {
 		
 		for (int i=0;i<average.length;i++){
 			average[i] = average[i]/data.size();
+			max[i] = max[i] - average[i];
 		}
 
 		double[] weights = new double[8];
@@ -57,27 +59,38 @@ public class LinearUnit {
 
 		for (int i = 0; i < data.size(); i++) {
 			int correct = 0;
+			double MSE = 0;
 			double[] newWeights = new double[8];
 			double[] predictions = new double[i+1];
 			for (int w = 0; w<weights.length; w++){
 				double wupdate = 0;
 				for (int j = 0; j<= i; j++){
 					double[] sample = data.get(j);
+					boolean cont = true;
+					for (int jj = 0;jj<sample.length;jj++){
+						if (sample[jj] == -1){
+							cont = false;
+						}
+					}
+					if (!cont){
+						continue;
+					}
 					//for (int iii=0;iii<sample.length;iii++) System.out.print(sample[iii]+" ");
 					//System.out.println(sample);
 					if (w == 0){
-						predictions[j] = sumProduct(weights, sample,average);
+						predictions[j] = sumProduct(weights, sample,average,max);
 						System.out.println(((int) Math.round(predictions[j])) + " -> " + (int)sample[7]);
 						if ((Math.abs(predictions[j] - sample[7])) < 1){
 							correct++;
 							System.out.println("GOT ONE RIGHT");
 						}
+						MSE += Math.abs(predictions[j] - sample[7]);
 					}
 					int pred =(int) Math.round(predictions[j]);
 					if (w == 0){
 						wupdate += (- sample[7] + pred) * 1;
 					}else{
-						wupdate += (- sample[7] + pred) * (sample[w-1]/average[w-1]);							
+						wupdate += (- sample[7] + pred) * (sample[w-1] - average[w-1])/max[w-1];							
 					}
 
 				}
@@ -87,16 +100,28 @@ public class LinearUnit {
 			weights = newWeights.clone();
 			for (int iii=0;iii<weights.length;iii++) System.out.print(weights[iii]+" ");
 			System.out.println("\n------------------------");
-			System.out.println("RESULTS " + correct);
+			System.out.println("RESULTS " + correct +"\n Total Error = " + MSE/data.size());
 		}
 	}
+	
+	public static double sumProduct(double[] w, double [] data, double average[], double max[]){
+		double sum = w[0];
+		
+		for (int i = 1;i<8;i++){
+			//System.out.println(sum+"+=" + w[i] + " * " + data[i-1]);
+			sum += w[i] * (data[i-1]-average[i-1])/max[i-1];
+		}
+		//System.out.println("F:"+sum);
+		return sum;
+	}
+	
 
 	public static double sumProduct(double[] w, double [] data, double average[]){
 		double sum = w[0];
 		
 		for (int i = 1;i<8;i++){
 			//System.out.println(sum+"+=" + w[i] + " * " + data[i-1]);
-			sum += w[i] * data[i-1]/average[i-1];
+			sum += w[i] * (data[i-1]/average[i-1]);
 		}
 		//System.out.println("F:"+sum);
 		return sum;
