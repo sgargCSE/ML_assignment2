f9c343dbb2715987c11b4f3b82301eb221dc88af
sgargCSE <sgarg@cse.unsw.edu.au>
2014-05-22 17:59:41 +1000

    Computes on variance

--- src/LinearUnit.java
+++ src/LinearUnit.java
@@ -47,8 +47,25 @@ public class LinearUnit {
 		for (int i=0;i<average.length;i++){
 			average[i] = average[i]/data.size();
 			max[i] = max[i] - average[i];
+//			System.out.println("AVG = " + average[i] + " ------ " + max[i]);
 		}
-
+		
+		double[] var = new double[8];
+		for (int i=0;i<data.size();i++){
+			double[] sample = data.get(i);
+			for (int j=0;j<sample.length;j++){
+				var[j] += (sample[j]-average[j])*(sample[j]-average[j]);
+			}
+		}
+		
+		for (int i = 0;i<var.length;i++){
+			var[i] = Math.sqrt(var[i]/(data.size()-1))*2;
+			System.out.println(" ** "+ max[i] + " .... " + var[i]);
+		}
+//if (0==0) return;
+double mm = 10000;
+double mm2 = 0;
+//for (double learning = 0.001; learning < .7; learning+= .001){
 		double[] weights = new double[8];
 		Random r = new Random(100);
 		for (int i = 0;i<8;i++){
@@ -56,14 +73,17 @@ public class LinearUnit {
 			weights[i] = 0;
 			//System.out.printf("%.2f ", weights[i]);
 		}
+		
 		//System.out.println();
-
+		double MSE_PREV = 0;
+		double learning = 0.497;
 		for (int i = 0; i < data.size(); i++) {
 			int correct = 0;
+			
 			double MSE = 0;
 			double[] newWeights = new double[8];
 			double[] predictions = new double[i+1];
-			for (int w = 0; w<weights.length; w++){
+			for (int w = 0; w<weights.length; w++){	
 				double wupdate = 0;
 				for (int j = 0; j<= i; j++){
 					double[] sample = data.get(j);
@@ -79,7 +99,7 @@ public class LinearUnit {
 					//for (int iii=0;iii<sample.length;iii++) System.out.print(sample[iii]+" ");
 					//System.out.println(sample);
 					if (w == 0){
-						predictions[j] = sumProduct(weights, sample,average,max);
+						predictions[j] = sumProduct(weights, sample,average,max,var);
 						DecimalFormat df = new DecimalFormat("#.##");
 						System.out.println(df.format(predictions[j])+ " -> " + (int)sample[7]);
 						if ((Math.abs(predictions[j] - sample[7])) < 1){
@@ -92,18 +112,52 @@ public class LinearUnit {
 					if (w == 0){
 						wupdate += (- sample[7] + pred) * 1;
 					}else{
-						wupdate += (- sample[7] + pred) * (sample[w-1] - average[w-1])/max[w-1];							
+						wupdate += (- sample[7] + pred) * (sample[w-1] - average[w-1])/var[w-1];							
 					}
 
 				}
-				newWeights[w] = weights[w] - 0.09 * 1/(i+1) * wupdate;
+				if (MSE > MSE_PREV){
+					newWeights[w] = weights[w] - learning * .5 * 1/(i+1) * wupdate;
+					//learning *= 0.95;
+				}else{
+					newWeights[w] = weights[w] - learning * 1.05 * 1/(i+1) * wupdate;
+					//learning *= 1.05;
+				}
+				newWeights[w] = weights[w] - learning * 1/(i+1) * wupdate;
 				//System.out.printf("%.2f ", weights[w]);
 			}
 			weights = newWeights.clone();
-			for (int iii=0;iii<weights.length;iii++) System.out.print(weights[iii]+" ");
+//			for (int iii=0;iii<weights.length;iii++) System.out.print(weights[iii]+" ");
 			System.out.println("\n------------------------");
-			System.out.println("RESULTS " + correct +"\n Total Error = " + MSE/data.size());
+			MSE = MSE/data.size();
+			System.out.println("RESULTS("+i+") " + correct +"\n Total Error = " + MSE);
+			if (i == (data.size()-1)){
+				if (MSE < mm){
+					mm = MSE;
+	//				mm2 = learning;
+				}
+			}
+			MSE_PREV  = MSE;
 		}
+//}		
+//System.out.println ( "MIN ERROR = " + mm + " -> " + mm2);
+		//mpg = -1.5 * cylinders - 1 * accelleration + 0.75 * year + 1.0
+		
+		double error = 0;
+		for (int i = 0;i<data.size();i++){
+			double[] sample = data.get(i);
+			double pred = 1 + sample[1]*1.5 -1 * sample[5]  + .75 * sample[6];
+			error += Math.abs(pred - sample[7]);
+		}
+		System.out.println("MODEL Error = " + error/data.size());
+	}
+
+	public static double sumProduct(double[] w, double [] data, double average[], double max[], double var[]){
+		double sum = w[0];
+		for (int i = 1;i<8;i++){
+			sum += w[i] * (data[i-1]-average[i-1])/var[i-1];
+		}
+		return sum;
 	}
 	
 	public static double sumProduct(double[] w, double [] data, double average[], double max[]){
